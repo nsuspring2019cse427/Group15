6da781f84211683a820addee2e2de5bc72a8d06d
Akiba Amrin <45199108+akibamrin@users.noreply.github.com>
2019-03-17 19:20:16 +0600

    Tests after Partitioning the input domain for GameSounds
    Tests after Partitioning the input domain for GameSounds

--- Project01/Code/tests/GameSoundTest.java
+++ Project01/Code/tests/GameSoundTest.java
@@ -5,20 +5,50 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GameSoundTest {
+
+    /**
+     * INITIALIZATION
+     *
+     **/
     private GameSound testObject;
+    private GameSound testObjectWithWrongAudioFile;
+
 
     @Before
     public void setUp() throws Exception {
+        /**
+         * Partitioning the input domain into two parts
+         *
+         * 01 Valid audio input file
+         * 02 Invalid audio input file line
+         *
+         **/
         testObject = new GameSound("sounds//flap.wav");
+        testObjectWithWrongAudioFile = new GameSound("InvalidLink");
     }
 
     @After
     public void tearDown() throws Exception {
+        /**
+         * Discarding Objects
+         *
+         **/
         testObject = null;
+        testObjectWithWrongAudioFile = null;
     }
 
     @Test
     public void audioFeedbackTest() {
+        /**
+         * Assert for valid input audio file
+         *
+         **/
         assertTrue(testObject.playAudioFeedback());
+
+        /**
+         * Assert for Invalid input audio file
+         *
+         **/
+        assertFalse(testObjectWithWrongAudioFile.playAudioFeedback());
     }
 }
