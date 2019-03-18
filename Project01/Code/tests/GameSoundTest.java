4a5546f15576f4c628c5ea14bc2f8139a3283ee6
Muhammad Sheehab <sheehabx@gmail.com>
2019-03-18 23:46:51 +0600

    Added parameterized test and Finalized GameSoundTest

--- Project01/Code/tests/GameSoundTest.java
+++ Project01/Code/tests/GameSoundTest.java
@@ -2,8 +2,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+
+
+@RunWith(Parameterized.class)
 public class GameSoundTest {
 
     /**
@@ -11,44 +19,65 @@ public class GameSoundTest {
      *
      **/
     private GameSound testObject;
-    private GameSound testObjectWithWrongAudioFile;
+    private String inputFile;
+    private boolean expected;
 
 
     @Before
     public void setUp() throws Exception {
         /**
-         * Partitioning the input domain into two parts
+         * Creating a new object each time a test is run
+         * from the input space partition
          *
-         * 01 Valid audio input file
-         * 02 Invalid audio input file line
-         *
-         **/
-        testObject = new GameSound("sounds//flap.wav");
-        testObjectWithWrongAudioFile = new GameSound("InvalidLink");
+         */
+        testObject = new GameSound(inputFile);
     }
 
     @After
     public void tearDown() throws Exception {
         /**
-         * Discarding Objects
+         * Discarding Objects after each call
          *
          **/
         testObject = null;
-        testObjectWithWrongAudioFile = null;
+    }
+
+    /**
+     * Constructor of each test with different values
+     *
+     **/
+    public GameSoundTest(String inputFile, Boolean expected) {
+        this.inputFile = inputFile;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection primeNumbers() {
+
+        /**
+         * Partitioning the input domain into two parts
+         * 
+         * 01 Valid audio input file
+         * 02 Invalid audio input file line
+         *
+         **/
+        return Arrays.asList(new Object[][] {
+                { "sounds//die.wav", true },
+                { "sounds//point.wav", true },
+                { "dsfsdfsdfsd", false },
+                { "sounds//flap.wav", true },
+                { "Invalid-file", false }
+        });
     }
 
     @Test
-    public void audioFeedbackTest() {
-        /**
-         * Test for valid input audio file
-         *
-         **/
-        assertTrue(testObject.playAudioFeedback());
+    public void playAudioFeedbackTest() {
 
         /**
-         * Test for Invalid input audio file
+         * Test for every input audio file
          *
          **/
-        assertFalse(testObjectWithWrongAudioFile.playAudioFeedback());
+        assertEquals(expected, testObject.playAudioFeedback());
     }
+
 }
