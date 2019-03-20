ce1af4245795af61a275a703662e7fea0be31887
Muhammad Sheehab <sheehabx@gmail.com>
2019-03-20 21:48:42 +0600

    Completed Unit Testing for all Methods, TestSuite

--- Project01/Code/tests/BirdFlapTest.java
+++ Project01/Code/tests/BirdFlapTest.java
@@ -8,17 +8,22 @@ import static org.junit.jupiter.api.Assertions.*;
 @Ignore("Tests are not feasible for this class")
 class BirdFlapTest {
 
+    private BirdFlap birdFlapTest;
+
+    GamePanel panel = new GamePanel();
+    Component bird = new Component(0, 0, 700, 490, "images//bird.png");
 
     @BeforeEach
     void setUp() {
-
+        birdFlapTest = new BirdFlap(panel, bird);
     }
 
     @AfterEach
     void tearDown() {
-
+        birdFlapTest = null;
     }
 
+    @Ignore("Not feasible for this method")
     @Test
     void run() {
 
