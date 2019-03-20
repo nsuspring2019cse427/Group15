aa1352f7f3f4d24749acabd8b5f7e8bb8b947d24
Akiba Amrin <45199108+akibamrin@users.noreply.github.com>
2019-03-20 21:41:42 +0600

    Adding all classes to test suite
    Finalised Unit Test

--- Project01/Code/tests/FlappyBirdTestSuite.java
+++ Project01/Code/tests/FlappyBirdTestSuite.java
@@ -3,7 +3,9 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runner.RunWith;
 
-
+@RunWith(JUnitPlatform.class)
+@SuiteDisplayName("JUnit FlappyBird Test Suite")
+@SelectClasses({BirdFlapTest.class, GamePanelTest.class, ComponentTest.class, GameSoundTest.class})
 
 public class FlappyBirdTestSuite {
     /**
