d98aeac1ccac8a9a8b51dc9c5ec6a3a1d4ea6e0c
Muhammad Sheehab <sheehabx@gmail.com>
2019-03-20 21:39:58 +0600

    Developed Game Test Suite

---
+++ Project01/Code/tests/FlappyBirdTestSuite.java
@@ -0,0 +1,13 @@
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+
+
+public class FlappyBirdTestSuite {
+    /**
+     * Test Suite for FlappyBird Test Classes
+     *
+     */
+}
