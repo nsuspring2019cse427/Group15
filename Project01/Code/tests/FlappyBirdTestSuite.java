60fdbef7e171a1483d3a41d27cbd1153a74f5bcc
Akiba Amrin <45199108+akibamrin@users.noreply.github.com>
2019-03-20 23:00:17 +0600

    Finalization of TestSuite

--- Project01/Code/tests/FlappyBirdTestSuite.java
+++ Project01/Code/tests/FlappyBirdTestSuite.java
@@ -8,8 +8,18 @@ import org.junit.runner.RunWith;
 @SelectClasses({BirdFlapTest.class, GamePanelTest.class, ComponentTest.class, GameSoundTest.class})
 
 public class FlappyBirdTestSuite {
+    
     /**
      * Test Suite for FlappyBird Test Classes
+     * Automation of the test suite, This class runs tests automatically from 
+     * all other test class that are added by SelectClasses Annotation. No extra codes required
+     */
+    
+    /**
+     * JUnit 5 was Used to build the test suite
+     * In Junit 5 Theory is Dead
+     * So, No presence of Theory. but the job of theory was done by Paramaterized Extension
+     *
      *
      */
 }
