f547655098d83003db7870c185a90607a0c4d189
Akiba Amrin <45199108+akibamrin@users.noreply.github.com>
2019-04-19 00:48:06 +0600

    Create GameGUITest.java

---
+++ Project01/Code/tests/GameGUITest.java
@@ -0,0 +1,18 @@
+import org.sikuli.script.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class GameGUITest {
+        
+          Screen s = new Screen();
+}
