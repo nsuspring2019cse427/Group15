97d6d5b268de5dbc2a80a80e4a5be44ddbe773e2
Akiba Amrin <45199108+akibamrin@users.noreply.github.com>
2019-04-23 00:09:30 +0600

    Finalizing GUI Test

--- Project01/Code/tests/GameGUITest.java
+++ Project01/Code/tests/GameGUITest.java
@@ -1,25 +1,23 @@
 
 import org.sikuli.script.Screen;
 
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
-import java.awt.*;
-import java.util.stream.Stream;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class GameGUITest  {
 
     private Screen scr;
+    private Main window;
 
     @BeforeEach
     void setUp() {
         scr = new Screen();
+
     }
 
     @AfterEach
@@ -30,7 +28,9 @@ public class GameGUITest  {
     @Test
     void GameInitializationScreenTest() {
         try {
+            Thread.sleep(500);
             scr.find("images/gameInitializationProof.png");
+            Thread.sleep(500);
             assertNotNull(scr.exists("images/gameInitializationProof.png"));
         } catch (Exception e) {}
     }
@@ -38,24 +38,53 @@ public class GameGUITest  {
     @Test
     void BirdOnScreenUITest() {
         try {
+            Thread.sleep(500);
             scr.find("images/birdScr.png");
+            Thread.sleep(500);
             assertNotNull(scr.exists("images/birdScr.png"));
         } catch (Exception e) {}
     }
-    @Test
 
+    @Test
     void PresenceOfPipeUpGUITest() {
         try {
+            Thread.sleep(500);
+            scr.keyUp();
+            Thread.sleep(500);
             scr.find("images/pipeUpScr.png");
             assertNotNull(scr.exists("images/pipeUpScr.png"));
         } catch (Exception e) {}
     }
 
     @Test
+    void GameOverGUITest() {
+        try {
+            Thread.sleep(500);
+            scr.keyUp();
+            Thread.sleep(100);
+            scr.find("images/overScr.png");
+           assertNotNull(scr.exists("images/overScr.png"));
+        } catch (Exception e) {}
+
+    }
+
+    @Test
     void PresenceOfPipeDownGUITest() {
         try {
+            Thread.sleep(500);
             scr.find("images/pipeDownScr.png");
-            assertNotNull(scr.exists("images/pipeDownScr.png"));
+            Thread.sleep(500);
+           assertNotNull(scr.exists("images/pipeDownScr.png"));
+        } catch (Exception e) {}
+    }
+
+    @Test
+    void GroundAvailableGUITest() {
+        try {
+            Thread.sleep(500);
+            scr.find("images/groundScr.png");
+            Thread.sleep(500);
+            assertNotNull(scr.exists("images/groundScr.png"));
         } catch (Exception e) {}
     }
 
